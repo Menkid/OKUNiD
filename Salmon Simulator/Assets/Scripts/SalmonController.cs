2fbd1f078a01ba8ce03d299377f0f8274ec3a3ee
Simon Narduzzi <simon.narduzzi@gmail.com>
2015-07-04 01:09:19 +0200

    Ajouté : Movement du Super-Saumon

---
+++ Salmon Simulator/Assets/Scripts/SalmonController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SalmonController : MonoBehaviour {
+
+	private Rigidbody2D body;
+	public float speed;
+	private bool faceRight = true;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		if (moveHorizontal < 0 && faceRight) {
+			Flip ();
+		}
+		if (moveHorizontal > 0 && !faceRight) {
+			Flip ();
+		}
+		Vector2 force = new Vector2 (moveHorizontal, moveVertical);
+		body.AddForce(force*speed);
+	}
+
+	void Flip(){
+		transform.localScale = new Vector3(transform.localScale.x *-1, transform.localScale.y, transform.localScale.z);
+		faceRight = !faceRight;
+	}
+}
