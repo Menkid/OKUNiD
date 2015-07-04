daa5fa10fe55cdad58248f3696971539064cefeb
Menkid <johan_cattin@hotmail.com>
2015-07-04 20:56:42 +0200

    weapon sound

--- Salmon Simulator/Assets/Scripts/Weapon.cs
+++ Salmon Simulator/Assets/Scripts/Weapon.cs
@@ -50,6 +50,7 @@ public class Weapon : MonoBehaviour {
             {
                 move.direction = shootRight ? new Vector2(1, 0) : new Vector2(-1, 0);
             }
+            AudioSource.PlayClipAtPoint(shoot, transform.position, 1);
         }
     }
 }
