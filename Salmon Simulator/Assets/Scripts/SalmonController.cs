140a3a761e2de6de2e0c8d5fdf3efa6f2404d106
Simon Narduzzi <simon.narduzzi@gmail.com>
2015-07-04 03:06:08 +0200

    Mis à jour des scripts de camera et de mouvement

--- Salmon Simulator/Assets/Scripts/SalmonController.cs
+++ Salmon Simulator/Assets/Scripts/SalmonController.cs
@@ -6,6 +6,7 @@ public class SalmonController : MonoBehaviour {
 	private Rigidbody2D body;
 	public float speed;
 	private bool faceRight = true;
+	public float frictionCoeff;
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D> ();
@@ -26,7 +27,10 @@ public class SalmonController : MonoBehaviour {
 		if (moveHorizontal > 0 && !faceRight) {
 			Flip ();
 		}
-		Vector2 force = new Vector2 (moveHorizontal, moveVertical);
+		float frictionX = frictionCoeff * body.velocity.x;
+		float frictionY = frictionCoeff * body.velocity.y;
+
+		Vector2 force = new Vector2 (moveHorizontal-frictionX, moveVertical-frictionY);
 		body.AddForce(force*speed);
 	}
 
