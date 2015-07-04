a1838c06aa79e9c477718cfaef0961a85487f4db
omacha <robin.weiskopf@gmail.com>
2015-07-04 13:59:50 +0200

    Added fish animations in Scene1

---
+++ Salmon Simulator/Assets/Scripts/Fish_Blink.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var eyeTimer : float;
+var blinkTimer : float;
+var randomFactor : float;
+
+private var currTime : float;
+private var blinking : boolean;
+
+function Start () {
+	currTime = eyeTimer + Random.value * randomFactor;
+	blinking = false;
+}
+
+function Update () {
+	if(currTime > 0.0f) {
+		currTime -= Time.deltaTime;
+	} else {
+		if(blinking) {
+			GameObject.Find("Salmon_eyes_closed").GetComponent.<Renderer>().enabled = false;
+			currTime = eyeTimer + Random.value * randomFactor;
+			blinking = false;
+		} else {
+			GameObject.Find("Salmon_eyes_closed").GetComponent.<Renderer>().enabled = true;
+			currTime = blinkTimer;
+			blinking = true;
+		}
+	}
+}
---
+++ Salmon Simulator/Assets/Scripts/Fish_eyes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish_eyes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MakeVisible() {
+		GameObject.FindGameObjectWithTag ("Salmon_eyes_closed").GetComponent<Renderer>().enabled = true;
+	}
+
+	public void MakeInvisible() {
+		GameObject.FindGameObjectWithTag ("Salmon_eyes_closed").GetComponent<Renderer>().enabled = false;
+	}
+}
