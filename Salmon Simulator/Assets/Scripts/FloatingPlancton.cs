345dd0a35aff7fb820040bd53182dc7da75ad8d7
Simon Narduzzi <simon.narduzzi@gmail.com>
2015-07-04 19:52:42 +0200

    Added : Plancton animation and Plancton points

---
+++ Salmon Simulator/Assets/Scripts/FloatingPlancton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingPlancton : MonoBehaviour {
+
+	public float speed = 0.5f;
+	private Vector2 origin;
+	private Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+		origin = new Vector2 (body.position.x, body.position.y);
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		body.position = new Vector2 (origin.x, origin.y+speed*Mathf.Cos(Time.time));
+	}
+}
---
+++ Salmon Simulator/Assets/Scripts/PlanctonPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanctonPoint : MonoBehaviour {
+	public int points = 1;
+	public int lifeTime = 0;
+	public bool isEnemy = false;
+	
+	// Use this for initialization
+	void Start()
+	{
+		if (lifeTime == 0)
+		{
+			return;
+		}
+		Destroy(gameObject, lifeTime);
+	}
+}
