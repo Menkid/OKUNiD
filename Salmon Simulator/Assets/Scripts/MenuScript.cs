d088dfc4fc7c4b5608638580d583a539192cb5e9
Simon Narduzzi <simon.narduzzi@gmail.com>
2015-07-05 15:41:47 +0200

    Fixed Menu and Intro

--- Salmon Simulator/Assets/Scripts/MenuScript.cs
+++ Salmon Simulator/Assets/Scripts/MenuScript.cs
@@ -5,16 +5,13 @@ using System.Collections;
 public class MenuScript : MonoBehaviour {
 
 	public Canvas quitMenu;
-	public Canvas Intro;
 	public Button play;
 	public Button quit;
-	public Button LetsGO;
 
 
 	// Use this for initialization
 	void Start () {
 		quitMenu = quitMenu.GetComponent<Canvas> ();
-		Intro = Intro.GetComponent<Canvas> ();
 		play = play.GetComponent<Button> ();
 		quit = quit.GetComponent<Button> ();
 		quitMenu.enabled = false;
@@ -40,7 +37,5 @@ public class MenuScript : MonoBehaviour {
 		Application.Quit ();
 	}
 
-	public void OnLetsGo(){
-		Application.LoadLevel (3);
-	}
+
 }
