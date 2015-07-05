78e4c7efb294bf0d82df3d6a3cf318580a7e8b07
Simon Narduzzi <simon.narduzzi@gmail.com>
2015-07-05 16:48:10 +0200

    game over menu

--- Salmon Simulator/Assets/Scripts/GAMEOVER.cs
+++ Salmon Simulator/Assets/Scripts/GAMEOVER.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
 
 public class GAMEOVER : MonoBehaviour {
-
+	public int wait;
 	// Use this for initialization
 	void Start () {
-	
+		if (wait <= 0) {
+			wait = 10;
+		}
+		Thread.Sleep(wait*1000);
 	}
 	
 	// Update is called once per frame
