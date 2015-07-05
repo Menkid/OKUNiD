f7282a4361ed3f6dc14d2f647fa8069515bd57d9
Menkid <johan_cattin@hotmail.com>
2015-07-05 14:57:20 +0200

    script to make something floating at the seed we want and the direction we want

---
+++ Salmon Simulator/Assets/Scripts/generics/Floating.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floating : MonoBehaviour
+{
+    public Move movement;
+    public float xFactor = 1;
+    public float yFactor = 1;
+    public float xSpeed = 1;
+    public float ySpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
