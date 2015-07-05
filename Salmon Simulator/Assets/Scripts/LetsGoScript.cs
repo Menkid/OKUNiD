16dfbdf28f5a0e062afd4453e4b772317922bcbd
Simon Narduzzi <simon.narduzzi@gmail.com>
2015-07-05 15:42:23 +0200

    Scripts LetsGoButton

---
+++ Salmon Simulator/Assets/Scripts/LetsGoScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LetsGoScript : MonoBehaviour {
+	public Canvas introCanvas;
+	public Button LetsGO;
+	
+	
+	// Use this for initialization
+	void Start () {
+		introCanvas = introCanvas.GetComponent<Canvas> ();
+		LetsGO = LetsGO.GetComponent<Button> ();
+
+	}
+	public void OnLetsGo(){
+		Application.LoadLevel (3);
+	}
+}
